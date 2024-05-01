97dcb65063066ea5eac84cce098d0fcedf7cdf11
Gabriel Sampaio <gabrielmasampaio@gmail.com>
2024-05-01 00:49:52 -0400

    fix index file

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,4 @@
 const express = require("express")
-const dbConfig = require("../database/config")
-const mongoose = require("mongoose")
 const cors = require("cors")
 const {rateLimit} = require("express-rate-limit")
 const routes = require('./routes/routes');
@@ -19,27 +17,6 @@ app.use(rateLimit({
 //Set routes
 app.use(routes)
 
-async function connectToDatabase() {
-  try {
-    await mongoose.connect(dbConfig.uri);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
-  }
-}
-
-connectToDatabase()
-    .then(() => {
-      // Start the server after the database connection is established
-      app.listen(3000, () => {
-        console.log("Server is running on port 3000");
-      });
-    })
-    .catch((error) => {
-      console.error("Error starting server:", error);
-      process.exit(1);
-    });
 
 
 module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,5 +1,25 @@
 require('dotenv').config();
 const app = require('./app')
+const mongoose = require("mongoose");
+const dbConfig = require("./database/config");
 const port = process.env.PORT || '8000'
 
-app.listen(port, () => console.log(`REST API listening on port :${port}`))
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(dbConfig.uri);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+}
+
+connectToDatabase()
+    .then(() => {
+      // Start the server after the database connection is established
+      app.listen(port, () => console.log(`REST API listening on port :${port}`))
+    })
+    .catch((error) => {
+      console.error("Error starting server:", error);
+      process.exit(1);
+    });
